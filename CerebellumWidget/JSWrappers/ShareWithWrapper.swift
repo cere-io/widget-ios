871488f8b75ac09a86caf7b11d10e12a0b50679a
Konstantin Svirsky <k.svirsky@funler.com>
2019-02-28 11:45:59 +0300

    Added verification of installed apps.
    NOTE: App. will require to add 4 url schemes to its Info.plist

--- CerebellumWidget/JSWrappers/ShareWithWrapper.swift
+++ CerebellumWidget/JSWrappers/ShareWithWrapper.swift
@@ -14,6 +14,12 @@ class ShareWithWrapper : JsProtocolWithResponse {
     override func handleEvent(widget: CerebellumWidget, data: AnyObject, responseCallback: ResponseCallback) {
         let json = JSON(data);
         let appId = json["app"]["id"].string;
+        
+        if (!appIsInstalled(appId, widget)) {
+            return;
+        }
+        
+        
         if (["facebook", "twitter"].contains(appId)) {
             let controller = SLComposeViewController(forServiceType: json["app"]["iosId"].string);
         
@@ -22,14 +28,19 @@ class ShareWithWrapper : JsProtocolWithResponse {
             widget.parentController!.present(controller!, animated: true, completion: nil);
         } else {
             var url: URL? = nil;
+            guard let params = json["data"].stringValue.addingPercentEncoding(withAllowedCharacters: .alphanumerics) else {
+                print("shareWithWrapper: incorrect data provided.");
+
+                return;
+            };
             
             if (appId == "telegram") {
-                url = URL(string: "tg://msg?text=\(json["data"].string!)");
+                url = URL(string: "tg://msg?text=\(params)");
             } else if (appId == "instagram") {
-                url = URL(string: "instagram://msg?text=\(json["data"].string!)");
+                url = URL(string: "instagram://msg?text=\(params)");
             }
             
-            if (url != nil /* && UIApplication.shared.canOpenURL(url!) */) {
+            if (url != nil) {
                 if #available(iOS 10.0, *) {
                     UIApplication.shared.open(url!, options: [:], completionHandler: nil);
                 } else {
@@ -39,5 +50,32 @@ class ShareWithWrapper : JsProtocolWithResponse {
         }
         
         responseCallback?(nil);
+    }
+    
+    private func appIsInstalled(_ appId: String?, _ widget: CerebellumWidget) -> Bool {
+        var url = "";
+        
+        switch appId {
+        case "telegram":
+            url = "tg";
+        case "instagram":
+            url = "instagram";
+        case "facebook":
+            url = "fb";
+        case "twitter":
+            url = "twitter";
+        default:
+            return false;
+        }
+        
+        if (!UIApplication.shared.canOpenURL(URL(string: "\(url)://app")!)) {
+            let alert = UIAlertController(title: "Message", message: "You do not have this application. Download it in App Store to share our url.", preferredStyle: UIAlertControllerStyle.alert);
+            
+            alert.addAction(UIAlertAction(title: "Close", style: UIAlertActionStyle.default, handler: nil));
+            
+            widget.parentController!.present(alert, animated: true, completion: nil);
+        }
+        
+        return true;
     }
 }
