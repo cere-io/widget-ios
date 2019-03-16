4909878121fcf02c6e3d950268c6023f89f08f4b
Konstantin Svirsky <k.svirsky@funler.com>
2019-03-17 01:22:25 +0300

    Added example app for the widget.

---
+++ CerebellumWidgetExample/CerebellumWidgetExample/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CerebellumWidgetExample
+//
+//  Created by C on 2/13/19.
+//  Copyright © 2019 Funler. All rights reserved.
+//
+
+import UIKit
+import CerebellumWidget
+
+class ViewController: UIViewController {
+    var crbWidget = CerebellumWidget();
+
+    @IBAction func showButtonClicked(sender: Any) {
+        crbWidget.setMode(mode: WidgetMode.REWARDS);
+        crbWidget.show();
+    }
+
+    @IBAction func logoutButtonClicked(sender: Any) {
+        crbWidget.logout();
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        crbWidget.initAndLoad(parentController: self,
+                              userId: "test@example.com",
+                              appId: "239",
+                              sections: ["top_section_1", "top_section_2", "top_section_3"],
+                              env: Environment.STAGE);
+        crbWidget.show();
+    }
+}
